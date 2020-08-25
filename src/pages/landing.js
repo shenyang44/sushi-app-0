e38e3643db1ac3b0d35c4b3043e6408b5b2cfbe1
Phoong Shen Yang <shenyang_zx@hotmail.com>
2020-08-25 13:19:30 +0800

    converting func to class comp.

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -3,32 +3,34 @@ import { Container, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { FaUsers } from 'react-icons/fa';
 
-const Landing = () => {
-    return (
-        <Container>
-            <Row className='d-flex justify-content-around align-items-center' style={{ height: '90vh' }}>
-                <Col id='char-col' xs={8} md={4} >
-                    <Link className='icon-col' to='/team'>
-                        <div className='landing-icon'>
-                            <FaUsers />
-                        </div>
-                        <div>
-                            Team Test.
-                        </div>
-                    </Link>
-                </Col>
-                <Col xs={0} md={1}>
-                </Col>
-                <Col xs={8} md={4}>
-                    <Link className='icon-col p-4' to='/about'>
-                        <div>
-                            About the Game.
-                        </div>
-                    </Link>
-                </Col>
-            </Row>
-        </Container >
-    )
+class Landing extends React.Component {
+    render() {
+        return (
+            <Container>
+                <Row className='d-flex justify-content-around align-items-center' style={{ height: '90vh' }}>
+                    <Col id='char-col' xs={8} md={4} >
+                        <Link className='icon-col' to='/team'>
+                            <div className='landing-icon'>
+                                <FaUsers />
+                            </div>
+                            <div>
+                                Team Test.
+                            </div>
+                        </Link>
+                    </Col>
+                    <Col xs={0} md={1}>
+                    </Col>
+                    <Col xs={8} md={4}>
+                        <Link className='icon-col p-4' to='/about'>
+                            <div>
+                                About the Game.
+                            </div>
+                        </Link>
+                    </Col>
+                </Row>
+            </Container >
+        )
+    }
 }
 
 export default Landing;
